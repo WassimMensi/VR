052f1541db9935390deb818ba71efc76189b139a
QGonnon <150007582+QGonnon@users.noreply.github.com>
2025-06-17 16:31:56 +0200

    start of taupe

--- Assets/Scripts/ChasseTaupe.cs
+++ Assets/Scripts/ChasseTaupe.cs
@@ -1,12 +1,21 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class ChasseTaupe : MonoBehaviour
 {
+    [SerializeField] public List<GameObject> Taupes;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        for (int i = 0; i < Taupes.Count; i++)
+        {
+            GameObject taupe = Taupes[i];
+            if (taupe != null)
+            {
+                taupe.transform.position += new Vector3(0,-1,0);
+            }
+        }
     }
 
     // Update is called once per frame
