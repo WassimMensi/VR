52b1f692ba14214c0e81619de559c1bdbf2ecbe1
Nathaniel Cobbinah <nathaniel.cobbinah2@ten.com>
2025-06-17 16:09:14 +0100

    task: closing credits impl: done

---
+++ Assets/CreditScript.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class CreditScript : MonoBehaviour
+{
+
+    public float scrollSpeed = 40f; 
+
+    private  RectTransform rectTransform;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rectTransform.anchoredPosition  += new Vector2(0, scrollSpeed * Time.deltaTime);
+    }
+}
