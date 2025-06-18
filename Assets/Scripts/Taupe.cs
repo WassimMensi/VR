c37022a2807851e64d4373ea60831a6712aa15e8
QGonnon <150007582+QGonnon@users.noreply.github.com>
2025-06-18 10:10:59 +0200

    test hammer

--- Assets/Scripts/Taupe.cs
+++ Assets/Scripts/Taupe.cs
@@ -3,35 +3,42 @@ using UnityEngine;
 public class Taupe : MonoBehaviour
 {
     [SerializeField] float interval = 3.0f;
-    [SerializeField] float lifeTime = 5.0f;
+    [SerializeField] float life = 5.0f;
     [SerializeField] float spawnChance = 25.0f;
 
-    float timeLife = 0.0f;
-    float timeInterval = 0.0f;
+    float timeLeftLife;
+    float timeLeftSpawn;
     bool spawned=false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        GameObject taupeCorps = GameObject.Find(name + "/Diglett Body");
-        GameObject taupeNez = GameObject.Find(name + "/Diglett Nose");
-        taupeCorps.transform.position += new Vector3(0, -1, 0);
-        taupeNez.transform.position += new Vector3(0, -1, 0);
+        timeLeftLife = life;
+        timeLeftSpawn = interval;
+        despawn();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(spawned) timeLife -= Time.deltaTime;
-        else {
+        float delta = Time.deltaTime;
+        if (spawned) timeLeftLife -= delta;
+        else timeLeftSpawn -= delta;
+
+        if(timeLeftSpawn < 0)
+        {
             bool spawning = Random.Range(0, 100) < spawnChance;
-            spawn();
+            if(spawning) spawn();
+            timeLeftSpawn = interval;
         }
-        if (timeLife <= 0) despawn();
+
+        if (timeLeftLife < 0) {
+            timeLeftLife = life;
+            despawn();
+        }
     }
 
     void spawn()
     {
-        timeLife = lifeTime;
         spawned = true;
         GameObject taupeCorps = GameObject.Find(name + "/Diglett Body");
         GameObject taupeNez = GameObject.Find(name + "/Diglett Nose");
@@ -41,8 +48,6 @@ public class Taupe : MonoBehaviour
 
     void despawn()
     {
-        timeLife = 0.0f;
-        timeInterval = interval;
         spawned = false;
         GameObject taupeCorps = GameObject.Find(name + "/Diglett Body");
         GameObject taupeNez = GameObject.Find(name + "/Diglett Nose");
